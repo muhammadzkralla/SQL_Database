b82678c44a65da902dd91cfc41ee4d305121d27c
Zkrallah <muhammad.heshamyt@gmail.com>
2024-03-01 17:20:09 +0200

    PADS optimized

--- The PADS.sql
+++ The PADS.sql
@@ -18,41 +18,20 @@ Note: There will be at least two entries in the table for each type of occupatio
 SELECT
     CONCAT(
         `NAME`,
-        CASE
-            WHEN `OCCUPATION` = 'Actor' THEN '(A)'
-            WHEN `OCCUPATION` = 'Doctor' THEN '(D)'
-            WHEN `OCCUPATION` = 'Professor' THEN '(P)'
-            WHEN `OCCUPATION` = 'Singer' THEN '(S)'
-            ELSE ''
-        END
+        '(',
+        LEFT(`OCCUPATION`, 1),
+        ')'
     )
 FROM `OCCUPATIONS`
 ORDER BY `NAME` ASC;
 SELECT 
     CONCAT(
         'There are a total of ',
-        COUNT(*),
-        ' doctors.'
+        COUNT(`OCCUPATION`),
+        ' ',
+        LOWER(`OCCUPATION`),
+        's.'
     ) 
-FROM `OCCUPATIONS` WHERE `OCCUPATION` = 'Doctor';
-SELECT 
-    CONCAT(
-        'There are a total of ',
-        COUNT(*),
-        ' actors.'
-    ) 
-FROM `OCCUPATIONS` WHERE `OCCUPATION` = 'Actor';
-SELECT 
-    CONCAT(
-        'There are a total of ',
-        COUNT(*),
-        ' singers.'
-    ) 
-FROM `OCCUPATIONS` WHERE `OCCUPATION` = 'Singer';
-SELECT 
-    CONCAT(
-        'There are a total of ',
-        COUNT(*),
-        ' professors.'
-    ) 
-FROM `OCCUPATIONS` WHERE `OCCUPATION` = 'Professor';
+FROM `OCCUPATIONS`
+GROUP BY `OCCUPATION`
+ORDER BY COUNT(`OCCUPATION`) ASC;
